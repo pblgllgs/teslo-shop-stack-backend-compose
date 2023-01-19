e59c69aacfe9181552f3c941172edba0d1e01728
pblgllgs <pbl.gllgs@gmail.com>
2023-01-19 11:31:30 -0300

    branch prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,24 +17,24 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
-      // ssl: process.env.STAGE === 'prod',
-      // extra: {
-      //   ssl: process.env.STAGE === 'prod'
-      //         ? { rejectUnauthorized: false }
-      //         : null,
-      // },
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,      
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
 
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'), 
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     ProductsModule,
@@ -51,4 +51,4 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
   ],
 })
-export class AppModule {}
+export class AppModule { }
